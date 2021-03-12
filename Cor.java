babd16c4c340d56461508243a8153d5d101461f4
Deivison de alcantara santos <Deivisonalc02@gmail.com>
2021-03-12 14:18:30 -0300

    criacao da classe e do construtor

---
+++ Cor.java
@@ -0,0 +1,19 @@
+public class Cor{
+
+    private int red;
+    private int green;
+    private int blue;
+    private int luninosidade;
+	
+    public Cor(int red, int green, int blue) {
+		
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+
+        
+    }
+
+    
+
+}
