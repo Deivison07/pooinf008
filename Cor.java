7dc95e368526792ec76451f1c7d356b22bac6e5f
Davi Rocha Macedo Sousa <davirmsousa@hotmail.com>
2021-03-12 21:37:55 -0300

    adicionando get e set das cores

--- Cor.java
+++ Cor.java
@@ -1,19 +1,43 @@
-public class Cor{
-
+public class Cor {
     private int red;
     private int green;
     private int blue;
     private int luninosidade;
-	
-    public Cor(int red, int green, int blue) {
-		
-        this.red = red;
-        this.green = green;
-        this.blue = blue;
 
-        
+    private final int VALOR_MINIMO = 0;
+    private final int VALOR_MAXIMO = 255;
+    
+    public Cor(int red, int green, int blue) {
+        setRed(red);
+        setGreen(green);
+        setBlue(blue);
     }
 
-    
+    private boolean validarValorDaCor(int valor) {
+        return valor >= VALOR_MINIMO && valor <= VALOR_MAXIMO;
+    }
 
+    public void setRed(int red) {
+        this.red = validarValorDaCor(red) ? red : VALOR_MINIMO;
+    }
+
+    public void setGreen(int green) {
+        this.green = validarValorDaCor(green) ? green : VALOR_MINIMO;
+    }
+
+    public void setBlue(int blue) {
+        this.blue = validarValorDaCor(blue) ? blue : VALOR_MINIMO;
+    }
+
+    public int getRed() {
+        return this.red;
+    }
+
+    public int getGreen() {
+        return this.green;
+    }
+
+    public int getBlue() {
+        return this.blue;
+    }
 }
