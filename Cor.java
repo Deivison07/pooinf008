80812cfde472cc4498f039756ea57b9951e26fa6
Deivison de alcantara santos <Deivisonalc02@gmail.com>
2021-03-17 12:10:35 -0300

    correcao de implementacao de construtores

--- Cor.java
+++ Cor.java
@@ -11,15 +11,13 @@ public class Cor {
         setGreen(green);
         setBlue(blue);
     }
-    this(){
-    	Cor(0,0,0);
+    public Cor(){
+    	this(0,0,0);
     }
 
-    this(Cor instancia){
-    	Cor(instancia.getRed(), instancia.getGreen(), instancia.getBlue());
+    public Cor(Cor instancia){
+    	this(instancia.getRed(), instancia.getGreen(), instancia.getBlue());
     }
-
-
 
     private int obterValorValido(int valor) {
         return valor < VALOR_MINIMO ? VALOR_MINIMO : valor > VALOR_MAXIMO ? VALOR_MAXIMO : valor;
