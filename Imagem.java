871182808982801646d133fd2917e7b0e711b1bf
Dev-JosemarJunior <dev.josemarjunior@gmail.com>
2021-04-09 17:31:05 -0300

    Class Imagem inserted

--- Imagem.java
+++ Imagem.java
@@ -1,13 +1,41 @@
+
+
 public class Imagem {
     
+    private Cor cor[][];
+    private int altura;
+    private int largura;
 
-
-    public Imagem criarImagem(int altura, int largura){
-
-        
+    //
+    public Imagem(){
+        this.altura = 1;
+        this.largura = 1;
+        this.cor[this.altura][this.largura];
     }
 
+    public Imagem(int novaAltura, int novaLargura){
+        setAltura(novaAltura);
+        setLargura(novaLargura);
+        this.cor[novaAltura][novaLargura];
+        setAllWhite();
+    }
     
+    public void setAltura(int novaAltura){
+        this.altura = novaAltura;
+    }
 
+    public void setLargura(int novaLargura){
+        this.largura = novaLargura;
+    }
 
+    private void setAllWhite(){
+
+        for (int a = 0; a < this.altura; a++){
+            for (int l = 0; l < this.largura; l++){
+                //Usar aqui o atributo static geral pra cor branca.
+                this.cor[altura][largura] = new Cor (255, 255, 255);
+            }
+        }
+        
+    }
 }
