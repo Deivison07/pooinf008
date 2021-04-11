bdc1aa1f5002d935bf1e991705abc721a7b23608
Deivison de alcantara santos <Deivisonalc02@gmail.com>
2021-04-11 02:29:54 -0300

    criacao do metodo que cria uma instancia de cor cinza equivalente a passada como parametro

--- Cor.java
+++ Cor.java
@@ -82,6 +82,12 @@ public class Cor {
     public String toString() {
         return "#" + this.obterHexa(this.getRed()) + this.obterHexa(this.getGreen()) + this.obterHexa(this.getBlue());
     }
+
+    public gerarCinzaEquivalente(Cor instancia){
+        int cinza = instancia.getLuminosidade()
+        return new Cor(cinza,cinza,cinza)
+
+    }
     
     private String obterHexa(int valor) {
         final char caracteresHexa[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
